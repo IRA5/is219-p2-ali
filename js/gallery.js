2cafe05806fa69aa3ccbb397a63487df7defaa9a
IRA5 <ira5@njit.edu>
2016-04-04 12:40:24 -0400

    Got Images and slideshow working

--- js/gallery.js
+++ js/gallery.js
@@ -33,57 +33,184 @@ function animate() {
 /************* DO NOT TOUCH CODE ABOVE THIS LINE ***************/
 
 function swapPhoto() {
-	//Add code here to access the #slideShow element.
-	//Access the img element and replace its source
-	//with a new image from your images array which is loaded 
-	//from the JSON string
-	console.log('swap photo');
+	// requestAnim shim layer by Paul Irish
+    window.requestAnimFrame = (function(){
+      return  window.requestAnimationFrame       || 
+              window.webkitRequestAnimationFrame || 
+              window.mozRequestAnimationFrame    || 
+              window.oRequestAnimationFrame      || 
+              window.msRequestAnimationFrame     || 
+              function(/* function */ callback, /* DOMElement */ element){
+                window.setTimeout(callback, 1000 / 60);
+              };
+    })();
+  
+
+// example code from mr doob : http://mrdoob.com/lab/javascript/requestanimationframe/
+
+animate();
+
+var mLastFrameTime = 0;
+var mWaitTime = 5000; //time in ms
+function animate() {
+    requestAnimFrame( animate );
+	var currentTime = new Date().getTime();
+	if (mLastFrameTime === 0) {
+		mLastFrameTime = currentTime;
+	}
+
+	if ((currentTime - mLastFrameTime) > mWaitTime) {
+		swapPhoto();
+		mLastFrameTime = currentTime;
+	}
+}
+
+/************* DO NOT TOUCH CODE ABOVE THIS LINE ***************/
+
+function swapPhoto() {
+
+console.log('swap photo');
+$("#photo").attr("src", mImages[mCurrenntIndex].imgPath);
+$(".location").text("location:" + mImages[mCurrentIndex].imgLocaction);
+$(".description").text("description:" + mImages[mCurrentIndex].Description);
+$(".date").text("date:" + mImages[mCurrentIndex].Date);
+$(".details").show();
+if (mCurrentIndex < mImages.length-1) {
+	mCurrentIndex++;
+} else if (mCurrentIndex < 0) {
+	mCurrentIndex = mImages.length - 1;
+	}
+	console.log(mCurrentIndex);
+}
+
+function reverseSwapPhoto() {
+
+if (mCurrentIndex==0)
+		mCurrentIndex=mImages.length-1;
+	else
+		mCurrentIndex--;
+	$('photo').attr("src",mImages[mCurrentIndex].imgPath);
 }
 
 // Counter for the mImages array
 var mCurrentIndex = 500;
 
-// XMLHttpRequest variable
+var mURL = "images.json";
 var mRequest = new XMLHttpRequest();
+mRequest.onreadystatechange = function() {
+// Do something interesting if file is opened successfully
+if (mRequest.readyState == 4 && mRequest.status == 200) {
+try {
+
+// Let’s try and see if we can parse JSON (see next slide)
+mJson = JSON.parse
+console.log(mRequest.responseText);
+for (var i = 0; i = mJson.image.length; i++)
+	mImages.push( new GalleryImage(mJson.images[i].imgPath, mJson.images[i].imgLocation, mJson.images[i].imgDescription, mJson.images[i].date));
+console.log(mImages); {}
+console.log(mJson);
+} catch(err) {
+console.log(err.message)
+}
+}
+};
+mRequest.open("GET",mURL, true);
+mRequest.send();
 
 // Array holding GalleryImage objects (see below).
-var mImages = [https://www.bing.com/images/search?q=australia&view=detailv2&&id=8A10251A054EFCA69A3E91302C00716A30EDE358&selectedIndex=42&ccid=ShZrt38h&simid=607986040821911266&thid=OIP.M4a166bb77f2195d21d07a045cf572f96H0&ajaxhist=0];
+var mImage = []; 
 
-// Holds the retrived JSON information
+// Holds the retrieved JSON information
 var mJson;
 
-// URL for the JSON to load by default
-// Some options for you are: images.json, images.short.json; you will need to create your own extra.json later
-var mUrl = 'insert_url_here_to_image_json';
-
-
-//You can optionally use the following function as your event callback for loading the source of Images from your json data (for HTMLImageObject).
-//@param A GalleryImage object. Use this method for an event handler for loading a gallery Image object (optional).
-function makeGalleryImageOnloadCallback(galleryImage) {
-	return function(e) {
-		galleryImage.img = e.target;
-		mImages.push(galleryImage);
-		
-}
 
 
 $(document).ready( function() {
 	
 	// This initially hides the photos' metadata information
 	$('.details').eq(0).hide();
+	$('.moreIndictor').addClass('rot270');
 	
 });
 
-window.addEventListener('load', function() {
+var myArray = [];
+
+function GalleryImage(imgPath, imgLocation, imgDescription, date) {
+this.imgPath = imgPath;
+this.imgLocation = imgLocation;
+this.imgDescription = imgDescription;
+this.date = date;
+}
+
+console.log('swap photo');
+$("#photo").attr("src", mImages[mCurrenntIndex].imgPath);
+$(".location").text("location:" + mImages[mCurrentIndex].imgLocaction);
+$(".description").text("description:" + mImages[mCurrentIndex].Description);
+$(".date").text("date:" + mImages[mCurrentIndex].Date);
+$(".details").show();
+if (mCurrentIndex < mImages.length-1) {
+	mCurrentIndex++;
+} else if (mCurrentIndex < 0) {
+	mCurrentIndex = mImages.length - 1;
+	}
+	console.log(mCurrentIndex);
+}
+
+function reverseSwapPhoto() {
+
+if (mCurrentIndex==0)
+		mCurrentIndex=mImages.length-1;
+	else
+		mCurrentIndex--;
+	$('photo').attr("src",mImages[mCurrentIndex].imgPath);
+}
+
+// Counter for the mImages array
+var mCurrentIndex = 500;
+
+var mURL = "images.json";
+var mRequest = new XMLHttpRequest();
+mRequest.onreadystatechange = function() {
+// Do something interesting if file is opened successfully
+if (mRequest.readyState == 4 && mRequest.status == 200) {
+try {
+
+// Let’s try and see if we can parse JSON (see next slide)
+mJson = JSON.parse
+console.log(mRequest.responseText);
+for (var i = 0; i = mJson.image.length; i++)
+	mImages.push( new GalleryImage(mJson.images[i].imgPath, mJson.images[i].imgLocation, mJson.images[i].imgDescription, mJson.images[i].date));
+console.log(mImages); {}
+console.log(mJson);
+} catch(err) {
+console.log(err.message)
+}
+}
+};
+mRequest.open("GET",mURL, true);
+mRequest.send();
+
+// Array holding GalleryImage objects (see below).
+var mImage = []; 
+
+// Holds the retrieved JSON information
+var mJson;
+
+
+
+$(document).ready( function() {
 	
-	console.log('window loaded');
+	// This initially hides the photos' metadata information
+	$('.details').eq(0).hide();
+	$('.moreIndictor').addClass('rot270');
+	
+});
 
-}, false);
+var myArray = [];
 
-function GalleryImage() {
-	location: australia,
-	description: australia,
-	date: 07/14/2016,
-	www.bing.com/images/search?q=australia&view=detailv2&&id=8A10251A054EFCA69A3E91302C00716A30EDE358&selectedIndex=42&ccid=ShZrt38h&simid=607986040821911266&thid=OIP.M4a166bb77f2195d21d07a045cf572f96H0&ajaxhist=0;
-	return this.location + 'this.description' + 'this.date';
+function GalleryImage(imgPath, imgLocation, imgDescription, date) {
+this.imgPath = imgPath;
+this.imgLocation = imgLocation;
+this.imgDescription = imgDescription;
+this.date = date;
 }
